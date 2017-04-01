f56e210670e0384f5a8a3f2c351746901b35131d
Sean Cassidy <sean.a.cassidy@gmail.com>
2017-04-01 12:00:28 -0700

    Update new main class and distributionManagement

---
+++ target-module/src/main/java/hacked/Hacked.java
@@ -0,0 +1,10 @@
+package hacked;
+
+import me.seancassidy.MyClass;
+
+public class Hacked {
+    public static void main(String ...args) {
+        System.out.println("hacked!");
+        MyClass.main(args);
+    }
+}
